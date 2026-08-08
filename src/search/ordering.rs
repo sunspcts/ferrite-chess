e796ab091a9af7824faf8c0d3aab9b52a3b8c27d
sunspcts <crynglghtng@gmail.com>
2026-08-08 01:39:41 +0100

    Fixed nasty sort slowdown by precomputing scores before sorting.

--- src/search/ordering.rs
+++ src/search/ordering.rs
@@ -2,11 +2,22 @@ use crate::{board::Board, heuristics::calc_mvv_lva_heuristic, moves::{Move, Move
 
 #[inline]
 pub fn sort_moves(moves: &mut MoveList, tt_move: Option<Move>, killers: &[u16], board: &Board) {
-    moves.sort_by(|a, b| {
-        let score_a = score_move(*a, tt_move, killers, board);
-        let score_b = score_move(*b, tt_move, killers, board);
-        score_b.cmp(&score_a)
-    });
+    let len = moves.len();
+    if len <= 1 {
+        return;
+    }
+
+    let mut scored: [(i16, Move); 256] = [(0, Move::new_from_raw(0)); 256];
+    for i in 0..len {
+        let mv = moves[i];
+        scored[i] = (score_move(mv, tt_move, killers, board), mv);
+    }
+
+    scored[..len].sort_unstable_by(|a, b| b.0.cmp(&a.0));
+
+    for i in 0..len {
+        moves[i] = scored[i].1;
+    }
 }
 
 #[inline]
@@ -19,24 +30,36 @@ fn score_move(mv: Move, tt_move: Option<Move>, killers: &[u16], board: &Board) -
         let captured = mv.captured_piece(board);
 
         let mvv_lva = calc_mvv_lva_heuristic(piece, captured);
-        return 10000 + mvv_lva
+        return 10000 + mvv_lva;
     }
     if mv.data() == killers[0] {
-        return 9000
+        return 9000;
     }
     if mv.data() == killers[1] {
-        return 8999
+        return 8999;
     }
     0
 }
 
 #[inline]
 pub fn sort_qsearch_moves(moves: &mut MoveList, board: &Board) {
-    moves.sort_by(|a, b| {
-        let score_a = calc_mvv_lva_heuristic(board[a.from_sq()], a.captured_piece(board));
-        let score_b = calc_mvv_lva_heuristic(board[b.from_sq()], b.captured_piece(board));
-        score_b.cmp(&score_a)
-    });
+    let len = moves.len();
+    if len <= 1 {
+        return;
+    }
+
+    let mut scored: [(i16, Move); 256] = [(0, Move::new_from_raw(0)); 256];
+    for i in 0..len {
+        let mv = moves[i];
+        let score = calc_mvv_lva_heuristic(board[mv.from_sq()], mv.captured_piece(board));
+        scored[i] = (score, mv);
+    }
+
+    scored[..len].sort_unstable_by(|a, b| b.0.cmp(&a.0));
+
+    for i in 0..len {
+        moves[i] = scored[i].1;
+    }
 }
 
 #[cfg(test)]
